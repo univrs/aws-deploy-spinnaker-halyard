a79de71a957ddc0592f76ca1ce731308db6e5d9f
ardeshir <ardeshir.org@gmail.com>
2018-10-16 19:59:53 -0500

    adding us-east-1

--- scripts/deploy_spinnaker.sh
+++ scripts/deploy_spinnaker.sh
@@ -42,7 +42,7 @@ done
 EKS_EC2_VPC_STACK_NAME="spin-eks-ec2-vpc"
 
 if [ -z "${REGION}" ]; then
-    REGION="us-west-2"
+    REGION="us-east-1"
 fi
 
 if [ "${USE_SSM_FOR_SECRETS}" == true ]; then
@@ -115,7 +115,7 @@ echo "Executing Halyard commands to create a Halyard configuration file"
 hal --color false config provider aws account add my-aws-account \
     --account-id ${ACCOUNT_ID} \
     --assume-role ${SPINNAKER_MANAGED_ROLE} \
-    --regions us-west-2
+    --regions us-east-1
 
 hal --color false config provider aws bakery edit --aws-vpc-id ${BAKING_VPC}
 hal --color false config provider aws enable
